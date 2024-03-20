79cb665c043cf321c9218ee4e21a61f9cbfdb02e
Here Perry <heremoana.perry@epsi.fr>
2024-03-20 16:45:23 +0100

    commit9

--- src/Composant/Page/Home/Home.jsx
+++ src/Composant/Page/Home/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
                         <img src={react} alt="logo react" className='iconFloat'/>
                     </div>
                     <h3 className='font-semibold text-2xl ms-9 lg:ms-0 text-center textReact'>Nation Sound</h3>
-                    <p className='mt-2 text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsum autem voluptatum facere. Optio facilis quia ipsa distinctio eveniet eos exercitationem nam omnis ad odio soluta assumenda, debitis quis molestiae!</p>
+                    <p className='mt-2 text-justify'>NationSound est un projet développé pour les ordinateurs de bureau ainsi que pour les smartphones. Il porte sur l'événement que la société LiveEvent organise : "NationSound". Vous trouverez ici les deux versions développées en React et React-Native.</p>
                     <p className='mt-2 text-right'><span className='underline underline-offset-4 font-semibold'>Version :</span> 1.0</p>
                 </Link>
             </motion.div>
@@ -54,7 +54,7 @@ function Home() {
                         <img src={symfony} alt="logo symfony" className='iconFloat'/>
                     </div>
                     <h3 className='font-semibold text-2xl ms-9 lg:ms-0 text-center textSymphony'>Live Event</h3>
-                    <p className='mt-2 text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsum autem voluptatum facere. Optio facilis quia ipsa distinctio eveniet eos exercitationem nam omnis ad odio soluta assumenda, debitis quis molestiae!</p>
+                    <p className='mt-2 text-justify'>LiveEvent est l'application web de la société du même nom. Ce projet vise à être le site principal de la société. Il sert à la fois de vitrine pour les utilisateurs et les consommateurs d'événements organisés par la société, mais aussi de back-office pour les sites et applications de la société, tels que NationSound, par exemple.</p>
                     <p className='mt-2 text-right'><span className='underline underline-offset-4 font-semibold'>Version :</span> 1.0</p>
                 </Link>
             </motion.div>
@@ -64,7 +64,9 @@ function Home() {
                         <img src={mysql} alt="logo mysql" className='iconFloat'/>
                     </div>
                     <h3 className='font-semibold text-2xl ms-9 lg:ms-0 text-center textMysql'>BDD</h3>
-                    <p className='mt-2 text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsum autem voluptatum facere. Optio facilis quia ipsa distinctio eveniet eos exercitationem nam omnis ad odio soluta assumenda, debitis quis molestiae!</p>
+                    <p className='mt-2 text-justify'>Ici, vous trouverez la documentation concernant la base de données du site LiveEvent. Vous y trouverez des explications sur les schémas qui regroupent plusieurs tables. 
+                    <br/>
+                    Attention : Si vous recherchez des documents sur la mise en place et le déploiement de la base de données, rendez-vous sur Live Event.</p>
                 </Link>
             </motion.div>
             <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{ duration:2 }} className='border-2 rounded-md p-6 mx-4 my-9 lg:basis-1/3'>
@@ -73,7 +75,7 @@ function Home() {
                         <img src={apiplatform} width="96" height="96" alt="logo apiplatform" className='iconFloat'/>
                     </div>
                     <h3 className='font-semibold text-2xl ms-9 lg:ms-0 text-center textApiplatform'>API</h3>
-                    <p className='mt-2 text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia ipsum autem voluptatum facere. Optio facilis quia ipsa distinctio eveniet eos exercitationem nam omnis ad odio soluta assumenda, debitis quis molestiae!</p>
+                    <p className='mt-2 text-justify'>L'API joue un rôle primordial dans la mise en place de la hiérarchisation entre le site principal et toutes les autres applications qui en découlent. C'est le pilier qui permet de partager les informations tout en sécurisant ces échanges. Vous trouverez ici comment elle a été installée et configurée.</p>
                 </Link>
             </motion.div>
             
